20dc2d3a4055f4a114479e29a90eba68a0e86767
xianhongtao <xianhongtao2022@outlook.com>
2025-04-01 19:56:17 +0800

    优化变量名称以提高可读性
    更改了代码中的变量名称，将 `n` 改为 `userInput` 和 `number`，以增强可读性。主函数中的输入提示信息进行了中文翻译，确保用户理解。`IsPrime` 方法的逻辑结构保持不变，但变量名称的更改使代码更具可读性。此外，将 `isPrime` 更改为 `isNumberPrime`，使变量名称更具描述性。判断素数的条件也进行了清晰化处理，整体结构和逻辑未变，主要优化了可读性。

--- PrimeNumberChecker/PrimeNumberChecker.cs
+++ PrimeNumberChecker/PrimeNumberChecker.cs
@@ -5,55 +5,40 @@
         for (; ; )
         {
             Console.WriteLine("请输入一个整数：");
-            int n = int.Parse(Console.ReadLine());
-            if (IsPrime(n))
+            int userInput = int.Parse(Console.ReadLine());
+            if (IsPrime(userInput))
             {
-                Console.WriteLine($"{n}是素数");
+                Console.WriteLine($"{userInput}是素数");
             }
             else
             {
-                Console.WriteLine($"{n}不是素数");
+                Console.WriteLine($"{userInput}不是素数");
             }
         }
     }
-    static Boolean IsPrime(int n)
+    static Boolean IsPrime(int number)
     {
-        int maxDivisorDigit = (int)Math.Ceiling(Math.Sqrt(n / 10));
-        Boolean isPrime = true;
-        if (n <= 1)
+        int maxDivisorDigit = (int)Math.Ceiling(Math.Sqrt(number / 10));
+        Boolean isNumberPrime = true;
+        if (number <= 1)
         {
-            isPrime = false;
+            isNumberPrime = false;
         }
-        if (n % 2 == 0 ||
-            n % 3 == 0 ||
-            n % 5 == 0 ||
-            n % 7 == 0)
+        if ((number % 2 == 0) || (number % 3 == 0) || (number % 5 == 0) || (number % 7 == 0) )
         {
-            isPrime = false;
+            isNumberPrime = false;
         }
-        for (int i = 2; i <= maxDivisorDigit; i++)
+        for (int divisorIndex = 1; divisorIndex <= maxDivisorDigit; divisorIndex++)
         {
-            if (n % (i * 10 + 1) == 0 ||
-                n % (i * 10 + 3) == 0 ||
-                n % (i * 10 + 7) == 0 ||
-                n % (i * 10 + 9) == 0)
+            if ((number % (divisorIndex * 10 + 1) == 0) || (number % (divisorIndex * 10 + 3) == 0) || (number % (divisorIndex * 10 + 7) == 0) || (number % (divisorIndex * 10 + 9) == 0))
             {
-                isPrime = false;
+                isNumberPrime = false;
             }
         }
-        if (n == 2 ||
-            n == 3 ||
-            n == 5 ||
-            n == 7 ||
-            n == 11 ||
-            n == 13 ||
-            n == 17 ||
-            n == 19 ||
-            n == 23 ||
-            n == 29)
+        if ((number == 2) || (number == 3) || (number == 5) || (number == 7) || (number == 11) || (number == 13) || (number == 17) || (number == 19) || (number == 23) || (number== 29))
         {
-            isPrime = true;
+            isNumberPrime = true;
         }
-        return isPrime;
+        return isNumberPrime;
     }
 }
