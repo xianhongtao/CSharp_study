116b9801cffd7f5adbb10f84c97759cd262dd7cd
xianhongtao <xianhongtao2022@outlook.com>
2025-04-01 19:16:10 +0800

    更新项目配置，添加素数检查功能
    - 在 `.gitignore` 中添加 `MigrationBackup/` 和 `FodyWeavers.xsd` 的忽略规则。
    - 更新 `CSharp_study.sln` 的 Visual Studio 版本至 17.13.35828.75，并添加新项目 "Test"。
    - 在全局配置中添加新项目 "Test" 的构建配置。
    - 在 `PrimeNumberChecker.cs` 中添加 `PrimeNumberChecker` 类及其素数判断方法。
    - 删除 `Program.cs` 中的 "Hello, World!" 输出代码。

---
+++ PrimeNumberChecker/PrimeNumberChecker.cs
@@ -0,0 +1,59 @@
+﻿public class PrimeNumberChecker
+{
+    public static void Main()
+    {
+        for (; ; )
+        {
+            Console.WriteLine("请输入一个整数：");
+            int n = int.Parse(Console.ReadLine());
+            if (IsPrime(n))
+            {
+                Console.WriteLine($"{n}是素数");
+            }
+            else
+            {
+                Console.WriteLine($"{n}不是素数");
+            }
+        }
+    }
+    static Boolean IsPrime(int n)
+    {
+        int maxDivisorDigit = (int)Math.Ceiling(Math.Sqrt(n / 10));
+        Boolean isPrime = true;
+        if (n <= 1)
+        {
+            isPrime = false;
+        }
+        if (n % 2 == 0 ||
+            n % 3 == 0 ||
+            n % 5 == 0 ||
+            n % 7 == 0)
+        {
+            isPrime = false;
+        }
+        for (int i = 2; i <= maxDivisorDigit; i++)
+        {
+            if (n % (i * 10 + 1) == 0 ||
+                n % (i * 10 + 3) == 0 ||
+                n % (i * 10 + 7) == 0 ||
+                n % (i * 10 + 9) == 0)
+            {
+                isPrime = false;
+            }
+        }
+        if (n == 2 ||
+            n == 3 ||
+            n == 5 ||
+            n == 7 ||
+            n == 11 ||
+            n == 13 ||
+            n == 17 ||
+            n == 19 ||
+            n == 23 ||
+            n == 29)
+        {
+            isPrime = true;
+        }
+        return isPrime;
+    }
+}
