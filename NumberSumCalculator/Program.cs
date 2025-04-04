de25a942ba2d1113f20971bd62d1cbe3eed6ce75
xianhongtao <xianhongtao2022@outlook.com>
2025-04-04 10:40:26 +0800

    添加 NumberSumCalculator 项目及其配置
    在 CSharp_study.sln 中添加了 "NumberSumCalculator" 项目，并更新了相关的构建配置。
    在 NumberSumCalculator.csproj 文件中，定义了项目的基本结构，包括输出类型、目标框架、隐式使用和可空性设置。
    
    添加 NumberSumCalculator 项目及其实现
    
    在解决方案中添加了新的 `NumberSumCalculator` 项目，并配置了相关的构建设置。更新了项目文件以定义基本属性，包括输出类型和目标框架。实现了 `NumberSumCalculator` 类及其 `Main` 方法，支持输入读取和数字和的计算，包含多个辅助方法用于数字处理。

---
+++ NumberSumCalculator/Program.cs
@@ -0,0 +1,56 @@
+﻿public class NumberSumCalculator
+{
+    public static void Main(string[] args)
+    {
+        for (; ; )
+        {
+            string[] input = Console.ReadLine().Split();
+            long totalPages = long.Parse(input[0]);
+            int divisor = int.Parse(input[1]);
+            long sumResult, divisionResult;
+            divisionResult = totalPages / divisor;
+            sumResult = divisionResult / GetLoopLenth(divisor) * CalculateDigitSum(divisor);
+            divisionResult = divisionResult % GetLoopLenth(divisor);
+            for (int index = 1; divisionResult != 0; divisionResult--)
+            {
+                sumResult += GetDigit(index * divisor);
+                index++;
+            }
+            Console.WriteLine(sumResult);
+        }
+    }
+
+    static int CalculateDigitSum(long number)
+    {
+        int digit = 0, sum = 0;
+        digit = GetDigit(number);
+        int index = 1;
+        for (; GetDigit(index * digit) != 0; index++)
+        {
+            sum += GetDigit(index * digit);
+        }
+        return sum;
+    }
+    static int GetDigit(long number)
+    {
+        int digit = 0;
+        digit = Convert.ToInt32(number % 10);
+        return digit;
+    }
+    static long GetLoopLenth(long number)
+    {
+        int digit = 0;
+        int lenth = 1;
+        digit = Convert.ToInt32(number % 10);
+        digit = GetDigit(number);
+        for (; ; )
+        {
+            if (GetDigit(lenth * digit) == 0)
+            {
+                break;
+            }
+            lenth++;
+        }
+        return lenth;
+    }
+}
