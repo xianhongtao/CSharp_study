2c31a65196cd5a8d2a9bd9b370029b1de6b30da9
xianhongtao <xianhongtao2022@outlook.com>
2025-05-18 11:05:52 +0800

    重构皇后问题代码，优化变量和逻辑
    将变量 `n` 和 `soluations` 更改为 `queenCount` 和 `solutions`，并更新输入提示和读取逻辑。修改棋盘初始化为 `chessPlate`，并调整相关逻辑以适应新变量名。在 `Step` 和 `PlaceQween` 方法中更新循环条件和参数，修复数组初始化方式。删除不再需要的 `PrintChessPlate` 方法，整体清理和优化代码，提高可读性和一致性。

--- NQween/Program.cs
+++ NQween/Program.cs
@@ -1,79 +1,65 @@
-﻿int n, soluations;
+﻿int queenCount, solutions;
 Console.WriteLine($"有几个皇后？");
-n = Convert.ToInt32(Console.ReadLine());
-
-Boolean[,] chessPlateAttacked = new Boolean[n, n];
-
-soluations = 0;
-Step(chessPlateAttacked, 0);
-Console.WriteLine($"{soluations}");
-
-void Step(Boolean[,] _chessPlateAttacked, int currentRow)
+queenCount = Convert.ToInt32(Console.ReadLine());
+Boolean[,] chessPlate = new Boolean[queenCount, queenCount];
+solutions = 0;
+Step(chessPlate, 0);
+Console.WriteLine($"{solutions}");
+void Step(Boolean[,] chessPlate, int currentRow)
 {
-    for (int index = 0; index < n; index++)
+    for (int currentColumn = 0; currentColumn < queenCount; currentColumn++)
     {
-        if (!_chessPlateAttacked[currentRow, index])
+        if (!chessPlate[currentRow, currentColumn])
         {
-            Boolean[,] newPlate = _chessPlateAttacked;
-            if (currentRow == n - 1)
+            Boolean[,] newPlate = (Boolean[,])chessPlate.Clone();
+            if (currentRow == queenCount - 1)
             {
-                soluations++;
+                solutions++;
             }
             else
             {
-                Step(PlaceQween(newPlate, currentRow, index), currentRow + 1);
+                Step(PlaceQween(newPlate, currentRow, currentColumn), currentRow + 1);
             }
         }
     }
 }
-static Boolean[,] PlaceQween(Boolean[,] chessPlateAttacked, int x, int y)
+static Boolean[,] PlaceQween(Boolean[,] chessPlate, int x, int y)
 {
-    for (int i = 0; i < chessPlateAttacked.GetLength(0); i++)
+    for (int i = 0; i < chessPlate.GetLength(0); i++)
     {
-        chessPlateAttacked[x, i] = true;
+        chessPlate[x, i] = true;
     }
-    for (int i = 0; i < chessPlateAttacked.GetLength(1); i++)
+    for (int i = 0; i < chessPlate.GetLength(1); i++)
     {
-        chessPlateAttacked[i, y] = true;
+        chessPlate[i, y] = true;
     }
-    int[] loc = [x, y];
-    for (; loc[0] < chessPlateAttacked.GetLength(0) && loc[1] < chessPlateAttacked.GetLength(1);)
+    int[] position = [x, y];
+    for (; position[0] < chessPlate.GetLength(0) && position[1] < chessPlate.GetLength(1);)
     {
-        chessPlateAttacked[loc[0], loc[1]] = true;
-        loc[0]++;
-        loc[1]++;
+        chessPlate[position[0], position[1]] = true;
+        position[0]++;
+        position[1]++;
     }
-    loc = [x, y];
-    for (; loc[0] >= 0 && loc[1] < chessPlateAttacked.GetLength(1);)
+    position = [x, y];
+    for (; position[0] >= 0 && position[1] < chessPlate.GetLength(1);)
     {
-        chessPlateAttacked[loc[0], loc[1]] = true;
-        loc[0]--;
-        loc[1]++;
+        chessPlate[position[0], position[1]] = true;
+        position[0]--;
+        position[1]++;
     }
-    loc = [x, y];
-    for (; loc[0] < chessPlateAttacked.GetLength(0) && loc[1] >= 0;)
+    position = [x, y];
+    for (; position[0] < chessPlate.GetLength(0) && position[1] >= 0;)
     {
-        chessPlateAttacked[loc[0], loc[1]] = true;
-        loc[0]++;
-        loc[1]--;
+        chessPlate[position[0], position[1]] = true;
+        position[0]++;
+        position[1]--;
     }
-    loc = [x, y];
-    for (; loc[0] >= 0 && loc[1] >= 0;)
+    position = [x, y];
+    for (; position[0] >= 0 && position[1] >= 0;)
     {
-        chessPlateAttacked[loc[0], loc[1]] = true;
-        loc[0]--;
-        loc[1]--;
+        chessPlate[position[0], position[1]] = true;
+        position[0]--;
+        position[1]--;
     }
-    return chessPlateAttacked;
-}
-static void PrintChessPlate(Boolean[,] plate)
-{
-    for (int i = 0; i < plate.GetLength(0); i++)
-    {
-        for (int j = 0; j < plate.GetLength(1); j++)
-        {
-            Console.Write(plate[i, j] ? "1 " : "0 ");
-        }
-        Console.WriteLine();
-    }
+    return chessPlate;
 }
