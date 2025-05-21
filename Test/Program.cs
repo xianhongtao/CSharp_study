f31b4f781b9f3c0bacec43d5d1c0241271e57b7d
xianhongtao <xianhongtao2022@outlook.com>
2025-05-21 19:37:01 +0800

    删除字符串输出，添加循环长度计算方法
    添加了 `GetLoopLength` 方法，用于计算两个整数的循环长度，并在控制台输出结果。移除了原有的字符串定义和输出语句。

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,3 +1,23 @@
-﻿string text = "Java";
-Console.WriteLine($"Hello {text} today");
-Boolean[][]
+﻿Console.WriteLine(GetLoopLength(32, 24));
+static int GetLoopLength(int n, int x)
+{
+    if (n % x == 0 || x % n == 0)
+    {
+        if (n % x == 0 && n != 1)
+        {
+            n /= x;
+        }
+        if (x % n == 0 && x != 1)
+        {
+            x /= n;
+        }
+    }
+    if (n > x)
+    {
+        return n;
+    }
+    else
+    {
+        return x;
+    }
+}
