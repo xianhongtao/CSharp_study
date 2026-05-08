1a8eb9e50f9bf91b506eca9ec6cdc111381cab0d
xianhongtao <xianhongtao2022@outlook.com>
2026-05-08 17:27:12 +0800

    移除无用的数字求和函数及相关调用

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,11 +1,1 @@
-﻿Console.WriteLine(GetDigitSum(1231442));
-static int GetDigitSum(int number)
-{
-    int sum = 0;
-    while (number > 0)
-    {
-        sum += number % 10;
-        number /= 10;
-    }
-    return sum;
-}
+﻿
